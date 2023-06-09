9ec142412313db71e286ecf1e4b5b354ed07b4b2
Benjamin <shallifi@hotmail.com>
2023-06-08 21:59:55 -0400

    added department id to employees controller

--- app/controllers/departments_controller.rb
+++ app/controllers/departments_controller.rb
@@ -2,6 +2,7 @@ class DepartmentsController < ApplicationController
     def index
         @departments = Department.all
 
+        # add search functionality to index action /////////////////////////////////
         if params[:search].present?
             @departments = @departments.where("department_name LIKE ?", "%#{params[:search]}%")
         end
