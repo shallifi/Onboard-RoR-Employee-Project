e9290220618c704a3c201fcc7dadf359fcb69742
Benjamin <shallifi@hotmail.com>
2023-06-08 22:42:18 -0400

    seeded the departments

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,45 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+departments = [
+    'Accounting',
+    'Administration',
+    'Adoption',
+    'Behavioral Health',
+    'Background Screening',
+    'CAP/GAP',
+    'CAPTA',
+    'Case Management',
+    'Contracts/Grant',
+    'Data Entry',
+    'Development',
+    'ERAT',
+    'Family Finders',
+    'HIPPY/Achievers',
+    'Human Resources',
+    'Independent Living',
+    'Information Technology',
+    'Licensing',
+    'Operations',
+    'Placement',
+    'Quality Assurance',
+    'Records Room',
+    'Rev Max',
+    'Schoolhouse Link',
+    'Shelter/GroupHome',
+    'Training',
+    'Youth Prevention Service'
+  ]
+
+  agencies = [1,2,3,4] # 4 agencies
+  
+  # create departments for each agency
+  departments.each do |name|
+    agencies.each do |agency_id|
+    puts "Creating department: #{name}" # print to terminal
+    Department.create(department_name: name, agency_id: agency_id)
+    end
+  end
+  
+  p "Created #{Department.count} departments"
