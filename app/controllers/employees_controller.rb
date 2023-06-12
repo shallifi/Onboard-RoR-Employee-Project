fd4baf3e3690948873ce20e1628df3988aa7eccf
Benjamin <shallifi@hotmail.com>
2023-06-11 23:06:59 -0400

    added Office seed

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -26,7 +26,7 @@ class EmployeesController < ApplicationController
     end
 
     def employee_params
-        params.require(:employee).permit(:first_name, :last_name, :new_to_scc, :driving_for_position, :transporting_children, :attending_preservice, :start_date , :agency_id, :department_id )
+        params.require(:employee).permit(:first_name, :last_name, :new_to_scc, :driving_for_position, :transporting_children, :attending_preservice, :start_date , :agency_id, :department_id, :office_id )
     end
 
 
