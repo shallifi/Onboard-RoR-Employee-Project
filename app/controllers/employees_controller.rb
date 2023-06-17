d6a651840875f66e707a6e7c2006b5c60c94402a
Benjamin <shallifi@hotmail.com>
2023-06-17 00:08:06 -0400

    unable to POST to employees_needs table

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -13,6 +13,9 @@ class EmployeesController < ApplicationController
         @employee = Employee.create(employee_params)
         
         if @employee.save
+            needs = Need.where(id: employee_params[:need_ids])
+            # @employee.needs << Need.find(employee_params[:need_ids])
+            @employee.needs << needs
             render json: @employee, status: :created
         else
             render json: @employee.errors, status: :unprocessable_entity
@@ -26,7 +29,7 @@ class EmployeesController < ApplicationController
     end
 
     def employee_params
-        params.require(:employee).permit(:first_name, :last_name, :new_to_scc, :driving_for_position, :transporting_children, :attending_preservice, :start_date , :agency_id, :department_id, :office_id, :extension, :additional_info, :needs)
+        params.require(:employee).permit(:first_name, :last_name, :new_to_scc, :driving_for_position, :transporting_children, :attending_preservice, :start_date , :agency_id, :department_id, :office_id, :extension, :additional_info, need_ids: [])
     end
 
 
