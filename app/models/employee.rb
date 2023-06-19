cceaa1217c7542fd752545bd78df6dbd8acdad55
Benjamin <shallifi@hotmail.com>
2023-06-19 10:25:32 -0400

    added to Employee model

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -4,11 +4,16 @@ class Employee < ApplicationRecord
     belongs_to :agency
     belongs_to :department
     belongs_to :office
+    belongs_to :title
     has_and_belongs_to_many :needs, join_table: :employees_needs
 
     validates :agency, presence: true
     validates :department, presence: true
     validates :office, presence: true
+    validates :title, presence: true
+    validates :first_name, presence: true
+    validates :last_name, presence: true
+
 
     def full_name
         "#{self.first_name} #{self.last_name}"
--- app/models/title.rb
+++ app/models/title.rb
@@ -1,2 +1,7 @@
 class Title < ApplicationRecord
+
+    has_many :employees
+
+    # validates :title_name  , presence: true , uniqueness: true // this is the validation for the title name 
+
 end
